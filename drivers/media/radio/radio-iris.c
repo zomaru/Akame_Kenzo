a0791ff4a5298f2b8c80b0d3eaf2102be84cc4ad
Kamal Negi <kamaln@codeaurora.org>
2016-10-19 18:59:11 +0530

    radio-iris: check argument values before copying the data
    Check arguments passed in an ioctl before copying the data to kernel
    buffers. If user sends an erroneous data, data length more than expected,
    will lead to buffer overflow.
    
    Change-Id: I663e937806f38dc3b04c8d7662cd8b045facd12b
    Signed-off-by: Kamal Negi <kamaln@codeaurora.org>

--- drivers/media/radio/radio-iris.c
+++ drivers/media/radio/radio-iris.c
@@ -3806,6 +3806,7 @@ static int iris_vidioc_s_ext_ctrls(struct file *file, void *priv,
 	case V4L2_CID_PRIVATE_IRIS_SET_SPURTABLE:
 		memset(&spur_tbl_req, 0, sizeof(spur_tbl_req));
 		data = (ctrl->controls[0]).string;
+<<<<<<< HEAD
 		if (copy_from_user(&bytes_to_copy, &((ctrl->controls[0]).size),
 					sizeof(bytes_to_copy))) {
 			retval = -EFAULT;
@@ -3818,6 +3819,11 @@ static int iris_vidioc_s_ext_ctrls(struct file *file, void *priv,
 		}
 		spur_tbl_req.mode = tmp_buf[0];
 		spur_tbl_req.no_of_freqs_entries = tmp_buf[1];
+=======
+		bytes_to_copy = (ctrl->controls[0]).size;
+		spur_tbl_req.mode = data[0];
+		spur_tbl_req.no_of_freqs_entries = data[1];
+>>>>>>> 498ae988d894... radio-iris: check argument values before copying the data
 
 		if (((spur_tbl_req.no_of_freqs_entries * SPUR_DATA_LEN) !=
 					bytes_to_copy - 2) ||
